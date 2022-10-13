ab1b9f9cbc023af7986460765ede894b044078a1
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-10-12 21:38:10 -0300

    Add files via upload

--- Python/Jogos/adivinha.py
+++ Python/Jogos/adivinha.py
@@ -6,11 +6,20 @@ numero_secreto = 42
 
 chute = int(input("Digite o seu número: "))
 
+acertou = chute == numero_secreto #true or false
+
+chute_alto = chute > numero_secreto #true or false
+
+chute_baixo = chute < numero_secreto #true or false
+
 print("Você digitou", chute)
 
-if(numero_secreto == chute):
-    print("Você acertou!")
+if(acertou):
+    print("Parabéns! Você acertou!")
 else:
-    print("Você errou!")
+    if(chute_alto):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif(chute_baixo):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim de Jogo.")
