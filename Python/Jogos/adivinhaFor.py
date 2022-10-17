0c003dc28c0b90b3e88c05bc9c21694d9c16127e
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-10-17 20:07:54 -0300

    Add files via upload

--- Python/Jogos/adivinhaFor.py
+++ Python/Jogos/adivinhaFor.py
@@ -6,11 +6,15 @@ numero_secreto = 42
 
 total_de_tentativas = 3
 
-for rodada in range(1, total_de_tentativas + 1) :
+for rodada in range(1, total_de_tentativas + 1) : #Para rodada de 1 ate total de tentativas + 1 faça
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas)) #String interpolation
 
-    chute = int(input("Digite o seu chute: "))
+    chute = int(input("Digite o seu chute de 1 a 100: "))
+
+    if(chute < 0 or chute > 100):
+        print("Número fora da margem, digite um número entre 1 a 100")
+        continue
 
     acertou = chute == numero_secreto #true or false
 
@@ -20,6 +24,7 @@ for rodada in range(1, total_de_tentativas + 1) :
 
     if(acertou):
         print("Parabéns! Você acertou!")
+        break
     else:
         if(chute_alto):
             print("Você errou! O seu chute foi maior do que o número secreto.")
