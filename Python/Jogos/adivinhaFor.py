d1e5830b930c7fbff284fb773302ba5275ab4a9b
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-10-18 22:09:44 -0300

    Add files via upload

--- Python/Jogos/adivinhaFor.py
+++ Python/Jogos/adivinhaFor.py
@@ -1,10 +1,29 @@
+import random #importar biblioteca com funções de números aleatórios
+
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101) #Primeiro randow para importar a biblioteca e depois o nome da função que está nessa biblioteca.
 
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 10
+
+elif(nivel == 2):
+    total_de_tentativas = 6
+
+else:
+    total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1) : #Para rodada de 1 ate total de tentativas + 1 faça
 
@@ -23,13 +42,21 @@ for rodada in range(1, total_de_tentativas + 1) : #Para rodada de 1 ate total de
     chute_baixo = chute < numero_secreto #true or false
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertou e fez {} pontos".format(pontos))
         break
     else:
         if(chute_alto):
             print("Você errou! O seu chute foi maior do que o número secreto.")
+            if(rodada == total_de_tentativas):
+                print("O número secreto era {}, você fez {} pontos".format(numero_secreto, pontos))
+
         elif(chute_baixo):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+            if (rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
+
+        pontos_perdidos = abs(numero_secreto - chute) #Valor absoluto, sem sinal.
+        pontos = pontos - pontos_perdidos
 
 print("****")
 print("Fim!")
