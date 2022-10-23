acfdc981351e4b7d8f3f992ceab00c5adb8c33d1
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-10-23 13:30:24 -0300

    Add files via upload

--- Python/Jogos/forca.py
+++ Python/Jogos/forca.py
@@ -2,8 +2,27 @@
 def forca():
 
     print("*********************************")
-    print("Bem vindo ao jogo de Forca!")
+    print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False #Enforcou? Não
+
+    acertou = False #Acertou? Não
+
+    while(not enforcou and not acertou): #While só executa o bloco caso seja verdadeiro, sendo falso o python pula para o próximo comando
+
+        chute = input("Qual letra?: ")
+
+        posicao = 1
+        
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(chute, posicao))
+            posicao = posicao + 1
+
+        print("Jogando...")
 
     print("****")
     print("Fim!")
