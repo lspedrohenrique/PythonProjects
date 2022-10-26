119330652419613557e483a73cf5c02741950c25
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-10-26 18:22:21 -0300

    Add files via upload

--- Python/Jogos/forca.py
+++ Python/Jogos/forca.py
@@ -7,23 +7,27 @@ def forca():
 
     palavra_secreta = "banana"
 
+    letras_secretas = ["_", "_", "_", "_", "_", "_"]
+
     enforcou = False #Enforcou? Não
 
     acertou = False #Acertou? Não
 
+    print(letras_secretas)
+
     while(not enforcou and not acertou): #While só executa o bloco caso seja verdadeiro, sendo falso o python pula para o próximo comando
 
         chute = input("Qual letra?: ")
-        chute = chute.strip() #A função .strip corrige qualquer entrada errada com espaços a mais
+        chute = chute.strip() #A função .strip() corrige qualquer entrada errada com espaços a mais
 
-        posicao = 1
+        indice = 0
 
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()): #A Função .upper() transforma toda a string aplicada em caracteres maiúsculos 
-                print("Encontrei a letra {} na posição {}".format(chute, posicao))
-            posicao = posicao + 1
+                letras_secretas[indice] = letra.upper() #Atribuindo a letra na posição do índice 
+            indice = indice + 1
 
-        print("Jogando...")
+        print(letras_secretas)
 
     print("****")
     print("Fim!")
