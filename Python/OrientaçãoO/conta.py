512b14126952fd8b61af743fd1f299750b55ed58
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-11-16 23:47:11 -0300

    Add files via upload

--- Python/OrientaçãoO/conta.py
+++ Python/OrientaçãoO/conta.py
@@ -6,16 +6,21 @@ class Conta:
     # Para chamar no console e criar um objeto: conta = Conta(1, "Eric Clapton", 0.0, 500.0)
     def __init__(self, numero, titular, saldo, limite = 1000.0): #metodo constutor, executa a função automaticamente para definir os atributos
         print("Construindo objeto {}".format(self))
-        self.numero = numero
-        self.titual = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero #Usamos o dois underline antes do nome do atributo para declarar um atributo privado
+        self.__titual = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        #Encapsulado o acesso a atributos
 
     def extrato(self): #Metodo extrato
-        print("Saldo {} do titular {}".format(self.saldo, self.titual))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titual))
 
     def deposita(self, valor): #Metodo deposita
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor): #Metodo saca
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor) #self também serve para chamar um método
+        destino.deposita(valor)
