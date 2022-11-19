9bb8d279c49610673605d71433e68c5b80bb8d70
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-11-19 12:25:19 -0300

    Add files via upload

--- Python/OrientaçãoO/conta.py
+++ Python/OrientaçãoO/conta.py
@@ -1,26 +1,37 @@
-
-
 class Conta:
-    # self é a referência do endereço do objeto na memória
-    #como defini o limite padrão como 1000.0, apenas é preciso informar como parametro caso seja um limite diferente
-    # Para chamar no console e criar um objeto: conta = Conta(1, "Eric Clapton", 0.0, 500.0)
-    def __init__(self, numero, titular, saldo, limite = 1000.0): #metodo constutor, executa a função automaticamente para definir os atributos
-        print("Construindo objeto {}".format(self))
-        self.__numero = numero #Usamos o dois underline antes do nome do atributo para declarar um atributo privado
-        self.__titual = titular
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
-        #Encapsulado o acesso a atributos
 
-    def extrato(self): #Metodo extrato
-        print("Saldo {} do titular {}".format(self.__saldo, self.__titual))
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
-    def deposita(self, valor): #Metodo deposita
+    def deposita(self, valor):
         self.__saldo += valor
 
-    def saca(self, valor): #Metodo saca
+    def saca(self, valor):
         self.__saldo -= valor
 
     def transfere(self, valor, destino):
-        self.saca(valor) #self também serve para chamar um método
+        self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
