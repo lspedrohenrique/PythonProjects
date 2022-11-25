cac8721c3d3367a7ffd73b14bdc846830b0fed78
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-11-25 02:34:51 -0300

    Add files via upload

--- Python/OrientaçãoO2/modelo.py
+++ Python/OrientaçãoO2/modelo.py
@@ -2,14 +2,14 @@ class Filme:
     def __init__(self, nome, ano, duracao):
         self.__nome = nome.title()
         self.ano = ano
-        self.duraçao = duracao
+        self.duracao = duracao
         self.__likes = 0
 
     @property
     def likes(self):
         return self.__likes
 
-    def dar_like(self):
+    def dar_likes(self):
         self.__likes += 1
 
     @property
@@ -28,6 +28,24 @@ class Serie:
         self.temporadas = temporadas
         self.__likes = 0
 
-    def dar_like(self):
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
         self.__likes += 1
 
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(vingadores.nome)
+
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
