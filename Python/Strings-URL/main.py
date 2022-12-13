a000148e50a3cb12db0b6ddb3f79345d1430bdb9
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-12-13 19:35:48 -0300

    Add files via upload

--- Python/Strings-URL/main.py
+++ Python/Strings-URL/main.py
@@ -1,3 +1,10 @@
 #URL sendo representada por uma string
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
+
+#Fatiamento de Strings
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
