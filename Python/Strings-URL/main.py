52898c1777a2c45b48eaa6de5298e6b8208f12e5
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-12-15 21:45:21 -0300

    Add files via upload

--- Python/Strings-URL/main.py
+++ Python/Strings-URL/main.py
@@ -1,13 +1,22 @@
 #URL sendo representada por uma String
-url = "https://bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 
 #Método para retornar o índice de uma String
 indice_interrogacao = url.find('?')
 
 #Fatiamento de Strings
 url_base = url[:indice_interrogacao]
-print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+paramentro_busca = 'quantidade'
+indice_parametro = url_parametros.find(paramentro_busca)
+indice_valor = indice_parametro + len(paramentro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor) #a partir de qual posição você quer encontrar algo
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+    print(valor)
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+    print(valor)
