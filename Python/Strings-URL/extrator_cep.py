fdbc4d4bb9be4b278cc3d760e1906070f9df6421
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-12-20 19:02:39 -0300

    Add files via upload

---
+++ Python/Strings-URL/extrator_cep.py
@@ -0,0 +1,11 @@
+import re
+
+endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
+
+#Criando um padrão no Python, o metodo compile retorna um padrão, retorna o objeto pattern
+padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+#Buscando em uma String se o padrão criado acima foi encontrado, retorna o objeto match(encontrou o padrão), retorna o match ou none caso não seja encontrado o padrão
+busca = padrao.search(endereco)
+if busca:
+    cep = busca.group() #Retorna a substring encontrada no match acima
+    print(cep)
