bd7061e937654331d6487ddebc70497ca3cb1f63
lspedrohenrique <113181896+lspedrohenrique@users.noreply.github.com>
2022-12-20 21:17:36 -0300

    Add files via upload

--- Python/Strings-URL/extrator_cep.py
+++ Python/Strings-URL/extrator_cep.py
@@ -3,7 +3,7 @@ import re
 endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
 
 #Criando um padrão no Python, o metodo compile retorna um padrão, retorna o objeto pattern
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}") # '{}' são quantificadores
 #Buscando em uma String se o padrão criado acima foi encontrado, retorna o objeto match(encontrou o padrão), retorna o match ou none caso não seja encontrado o padrão
 busca = padrao.search(endereco)
 if busca:
